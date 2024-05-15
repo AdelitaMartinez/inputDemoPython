cdda16451b59c69a65a852c3b7362c30732313a0
Adelita Martinez <amartinez1013@cnm.edu>
2024-05-15 13:29:04 -0600

    Added psudocode

--- inputDemo.py
+++ inputDemo.py
@@ -2,3 +2,11 @@
 # Programmer: Adelita Martinez (amartinez1013@cnm.edu)
 # Date: 15 May 2024
 # Purpose: Practice getting input from the user
+
+# Get first number from user 
+
+# Get second number from user
+
+# Add numbers together 
+
+# Display result
