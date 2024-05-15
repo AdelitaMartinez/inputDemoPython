c5a62ea2bd4030e73c541f026c59a1103622148c
Adelita Martinez <amartinez1013@cnm.edu>
2024-05-15 13:35:11 -0600

    Added name variable

--- inputDemo.py
+++ inputDemo.py
@@ -3,6 +3,9 @@
 # Date: 15 May 2024
 # Purpose: Practice getting input from the user
 
+# Get person's name 
+name = input('Please enter your name: ')
+
 # Get first number from user
 num1 = input('Please enter a number: ') 
 
