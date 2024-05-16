9609c348d9e4854e5b812df3b7ad3522100ee981
Adelita Martinez <amartinez1013@cnm.edu>
2024-05-16 08:03:31 -0600

    Converted code to single line

--- inputDemo.py
+++ inputDemo.py
@@ -7,15 +7,20 @@
 name = input('Please enter your name: ')
 
 # Get first number from user
-num_str1 = input('Please enter a number: ') 
+# num_str1 = input('Please enter a number: ') 
 
 # Convert to number
 # Float converts values into floating point numbers
-num1 = float(num_str1)
+# num1 = float(num_str1)
+
+# Both these steps can be put together in one line like below:
+num1 = float(input('Please enter a number: '))
 
 # Get second number from user
-num_str2 = input('Please enter another number: ')
-num2 = float(num_str2)
+# num_str2 = input('Please enter another number: ')
+# num2 = float(num_str2)
+
+num2 = float(input('Please enter another number: '))
 
 # Add numbers together 
 sum = num1 + num2
